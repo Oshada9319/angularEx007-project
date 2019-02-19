501a055a2627212cf3c08df43d7cf5cf5920b4cc
Oshada Maheeshan <oshadamahee@gmail.com>
2019-02-19 14:39:38 +0530

    add Data Sync with Observable and Behavior Subject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,26 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { DateComponent } from './date/date.component';
-import { AddressCardComponent } from './address-card/address-card.component';
-import { ViewModule } from './view/view.module';
 import { TestService } from './test.service';
-import { HttpClientModule } from '@angular/common/http'
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    DateComponent,
-    AddressCardComponent
+    OneComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ViewModule,
     HttpClientModule
   ],
   providers: [
-    TestService
+    TestService,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
